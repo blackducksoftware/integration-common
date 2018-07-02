3b0b6cc011920cc0f07aea4b671c2c8b02ce7c3a
Eric Kerwin <ekerwin@kingofnerds.com>
2018-07-02 13:14:11 -0400

    fixing usage of capitalize fully

--- src/main/java/com/blackducksoftware/integration/util/EnumUtils.java
+++ src/main/java/com/blackducksoftware/integration/util/EnumUtils.java
@@ -28,7 +28,7 @@ import org.apache.commons.text.WordUtils;
 public class EnumUtils {
     public static String prettyPrint(final Enum<?> enumValue) {
         final String name = enumValue.name();
-        final String prettyName = WordUtils.capitalizeFully(name.toLowerCase().replace("_", " "));
+        final String prettyName = WordUtils.capitalizeFully(name.replace("_", " "));
         return prettyName;
     }
 
