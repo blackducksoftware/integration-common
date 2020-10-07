61b6e0b20770182aaecf0d8b54ae87c96ae2ebab
Eric Kerwin <ekerwin@synopsys.com>
2020-10-07 17:12:01 -0400

    static creators

--- src/main/java/com/synopsys/integration/util/ExcludedIncludedFilter.java
+++ src/main/java/com/synopsys/integration/util/ExcludedIncludedFilter.java
@@ -22,6 +22,7 @@
  */
 package com.synopsys.integration.util;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -35,24 +36,27 @@ public class ExcludedIncludedFilter {
     protected final Set<String> includedSet;
 
     /**
-     * Construct a filter with no excludes (and no includes).
+     * Construct a filter with no excludes or includes.
      */
-    public ExcludedIncludedFilter() {
-        this(Collections.emptyList(), Collections.emptyList());
+    public static ExcludedIncludedFilter CREATE_EMPTY() {
+        return new ExcludedIncludedFilter(Collections.emptyList(), Collections.emptyList());
     }
 
     /**
-     * Provide a comma-separated list of names to exclude and/or a comma-separated list of names to include. Exclusion rules always win.
+     * Provide a comma-separated string of values to exclude and/or a comma-separated string of values to include. Exclusion rules always win.
      */
-    public ExcludedIncludedFilter(String toExclude, String toInclude) {
-        excludedSet = createSetFromString(toExclude);
-        includedSet = createSetFromString(toInclude);
+    public static ExcludedIncludedFilter CREATE_FROM_COMMA_SEPARATED_STRINGS(String toExclude, String toInclude) {
+        return new ExcludedIncludedFilter(TokenizerUtils.createSetFromString(toExclude), TokenizerUtils.createSetFromString(toInclude));
     }
 
     /**
-     * Provide a list of names to exclude and/or a list of names to include. Exclusion rules always win.
+     * Provide a collection of values to exclude and/or a collection of values to include. Exclusion rules always win.
      */
-    public ExcludedIncludedFilter(List<String> toExcludeList, List<String> toIncludeList) {
+    public static ExcludedIncludedFilter CREATE_FROM_COLLECTIONS(Collection<String> toExclude, Collection<String> toInclude) {
+        return new ExcludedIncludedFilter(toExclude, toInclude);
+    }
+
+    protected ExcludedIncludedFilter(Collection<String> toExcludeList, Collection<String> toIncludeList) {
         excludedSet = new HashSet<>(toExcludeList);
         includedSet = new HashSet<>(toIncludeList);
     }
@@ -77,15 +81,6 @@ public class ExcludedIncludedFilter {
         } else {
             return willInclude(itemName);
         }
-    }
-
-    private Set<String> createSetFromString(String s) {
-        Set<String> set = new HashSet<>();
-        StringTokenizer stringTokenizer = new StringTokenizer(StringUtils.trimToEmpty(s), ",");
-        while (stringTokenizer.hasMoreTokens()) {
-            set.add(StringUtils.trimToEmpty(stringTokenizer.nextToken()));
-        }
-        return set;
     }
 
 }
