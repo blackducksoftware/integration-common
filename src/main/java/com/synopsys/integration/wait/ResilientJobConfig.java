acc3ce83280563dbaf261783e36e4403026b18ac
John Petrakis <petrakis@synopsys.com>
2022-09-14 14:30:57 -0400

    IDETECT-3475 | Change polling from constant to progessive wait intervals | jppetrakis

--- src/main/java/com/synopsys/integration/wait/ResilientJobConfig.java
+++ src/main/java/com/synopsys/integration/wait/ResilientJobConfig.java
@@ -14,10 +14,15 @@ import com.synopsys.integration.log.IntLogger;
 public class ResilientJobConfig {
     public static final Supplier<Long> CURRENT_TIME_SUPPLIER = System::currentTimeMillis;
 
+    private final String POLLING_INTERVAL_PROPERTY = "ConstantPollingInterval";
     private final IntLogger intLogger;
     private final long timeoutInSeconds;
     private final Supplier<Long> startTimeSupplier;
     private final int waitIntervalInSeconds;
+
+    private int prev1 = 1;
+    private int prev2 = 0;
+    private int max = 60;
 
     public ResilientJobConfig(IntLogger intLogger, long timeoutInSeconds, long startTime, int waitIntervalInSeconds) {
         this(intLogger, timeoutInSeconds, () -> startTime, waitIntervalInSeconds);
@@ -49,7 +54,28 @@ public class ResilientJobConfig {
     }
 
     public int getWaitIntervalInSeconds() {
-        return waitIntervalInSeconds;
+        String flatInterval = System.getProperty(POLLING_INTERVAL_PROPERTY);
+        if (null == flatInterval) {
+            return this.getWaitIntervalFibonacciInSeconds2();  // default progressive interval
+        } else if (flatInterval.matches("[0-9]+")){
+            return Integer.getInteger(POLLING_INTERVAL_PROPERTY).intValue(); // non-default constant polling interval
+        }
+        return waitIntervalInSeconds; // default 1 second interval
     }
+    
+    public int getWaitIntervalFibonacciInSeconds2() {
+        int now = 1;
+        if (prev2 == -1 && prev1 == 0) {
+            now = prev1 - prev2;
+        } else {
+            now = prev2 + prev1;
+        }
+        if (now >= max) {
+            return max;
+        }
 
+        prev2 = prev1;
+        prev1 = now;
+        return now;
+    }
 }
