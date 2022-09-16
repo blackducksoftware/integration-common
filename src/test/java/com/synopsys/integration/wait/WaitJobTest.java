81173fdf70c27c7719758cc6776612253937582a
John Petrakis <petrakis@synopsys.com>
2022-09-16 13:35:51 -0400

    IDETECT-3475 | Added test method in WaitJobTest for the progessive wait intervals | jppetrakis

--- src/test/java/com/synopsys/integration/wait/WaitJobTest.java
+++ src/test/java/com/synopsys/integration/wait/WaitJobTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 public class WaitJobTest {
     private final BufferedIntLogger testingLogger = new BufferedIntLogger();
     private final ResilientJobConfig waitJobConfig = new ResilientJobConfig(testingLogger, 5, ResilientJobConfig.CURRENT_TIME_SUPPLIER, 1);
+    private final ResilientJobConfig waitJobConfigFibonacciWaitIntervals = new ResilientJobConfig(testingLogger, 60, System.currentTimeMillis(), 1, true);
 
     @Test
     public void testTaskCompletesImmediately() throws IntegrationException, InterruptedException {
@@ -50,6 +51,35 @@ public class WaitJobTest {
         assertFalse(output.contains("Try #0"));
         assertTrue(output.contains("Try #1"));
         assertTrue(output.contains("Try #2"));
+
+        assertTrue(output.contains("holypants"));
+        assertTrue(output.contains("complete!"));
+        assertTrue(output.contains("not done yet"));
+    }
+
+    @Test
+    public void testTaskCompletesEventuallyFibonacciWaitIntervals() throws IntegrationException, InterruptedException {
+        WaitJobCondition waitJobCondition = new WaitJobCondition() {
+            private int count = 0;
+
+            @Override
+            public boolean isComplete() {
+                return ++count > 5;
+            }
+        };
+        boolean completed = WaitJob.waitFor(waitJobConfigFibonacciWaitIntervals, waitJobCondition, "holypants");
+
+        assertTrue(completed);
+
+        String output = testingLogger.getOutputString(LogLevel.INFO);
+        // this usage should have an increasing time delay interval for each iteration
+        // according to a modified Fibonacci sequence (1 2 3 5... rather than 1 1 2 3 5...
+        assertFalse(output.contains("Try #0"));
+        assertTrue(output.contains("Try #1") && output.contains("1 seconds"));
+        assertTrue(output.contains("Try #2") && output.contains("2 seconds"));
+        assertTrue(output.contains("Try #3") && output.contains("3 seconds"));
+        assertTrue(output.contains("Try #4") && output.contains("5 seconds"));
+        assertTrue(output.contains("Try #5") && output.contains("8 seconds"));
 
         assertTrue(output.contains("holypants"));
         assertTrue(output.contains("complete!"));
