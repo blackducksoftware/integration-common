4a5ac420adf6a477a0fb6b0a8778a13fdb97b974
Steve Billings <billings@synopsys.com>
2022-09-23 15:37:58 -0400

    style: copyright headers

--- src/main/java/com/synopsys/integration/wait/tracker/WaitIntervalTrackerConstant.java
+++ src/main/java/com/synopsys/integration/wait/tracker/WaitIntervalTrackerConstant.java
@@ -1,3 +1,10 @@
+/*
+ * integration-common
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.wait.tracker;
 
 public class WaitIntervalTrackerConstant implements WaitIntervalTracker {
